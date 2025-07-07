ef082ed2d008b7a12673b4416dff351a0b1b2859
“Alex <alexjcarter94@icloud.com>
2025-07-07 10:12:39 +0200

    Setup data entries needed to complete the game board and allow for choices from the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-console.log("Rock Paper Scissors Game Loaded");
+const choices = document.querySelectorAll('.choice'); // Stores all the choices in an array
+const userChoiceDisplay = document.querySelector('.user-choice'); //To be set once user clicks on a choice
+const computerChoiceDisplay = document.querySelector('.computer-choice'); //To be set once computer makes a choice
+const resultDisplay = document.querySelector('.result'); //To be set once the result(who the winner is) is determined
+const userScoreDisplay = document.querySelector('.user-score'); //To be set once the user score is updated
+const computerScoreDisplay = document.querySelector('.computer-score'); //To be set once the computer score is updated
+let userScore = 0; //Initial user score
+let computerScore = 0; //Initial computer score 
+
+
+
+
+
+console.log(choices[0].getAttribute('data-choice'));
